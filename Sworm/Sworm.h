de02418d22405c49b9a1ba033777afaa5b6721f0
Wilbert Ribeiro <wkelyson@gmail.com>
2015-10-30 18:10:37 -0200

    Initial Commit

---
+++ Sworm/Sworm.h
@@ -0,0 +1,19 @@
+//
+//  Sworm.h
+//  Sworm
+//
+//  Created by Wilbert Ribeiro on 30/10/15.
+//  Copyright © 2015 Wilbert Ribeiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Sworm.
+FOUNDATION_EXPORT double SwormVersionNumber;
+
+//! Project version string for Sworm.
+FOUNDATION_EXPORT const unsigned char SwormVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sworm/PublicHeader.h>
+
+
