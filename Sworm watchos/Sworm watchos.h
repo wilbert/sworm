520f2067dd8af2667dffebc45f78208962b3768c
Wilbert Ribeiro <wkelyson@gmail.com>
2016-03-23 18:17:49 -0300

    Adding Target for watch os.

---
+++ Sworm watchos/Sworm watchos.h
@@ -0,0 +1,19 @@
+//
+//  Sworm watchos.h
+//  Sworm watchos
+//
+//  Created by Wilbert Ribeiro on 3/23/16.
+//  Copyright © 2016 Wilbert Ribeiro. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for Sworm watchos.
+FOUNDATION_EXPORT double Sworm_watchosVersionNumber;
+
+//! Project version string for Sworm watchos.
+FOUNDATION_EXPORT const unsigned char Sworm_watchosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sworm_watchos/PublicHeader.h>
+
+
